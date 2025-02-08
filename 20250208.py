1db7bc0476d0f1d617baab31b726a3829a7ac7e2
LeeSohyun <102014723+sohyun703@users.noreply.github.com>
2025-02-08 21:09:37 +0900

    Add files via upload

---
+++ 20250208.py
@@ -0,0 +1,25 @@
+https: // www.acmicpc.net / problem / 10039
+
+total = 0
+
+for i in range(5):
+    tmp = int(input())
+
+    if tmp < 40:
+        tmp = 40
+
+    total += tmp
+
+print(total // 5)https: // www.acmicpc.net / problem / 10039
+
+total = 0
+
+for i in range(5):
+    tmp = int(input())
+
+    if tmp < 40:
+        tmp = 40
+
+    total += tmp
+
+print(total // 5)
